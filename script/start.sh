791c4ed916ba500a860bf0c00b6b8c0d1d3fe27f
Bryan Overduin <bryan.overduin@sendsteps.comn>
2023-08-12 00:28:10 +0200

    start script

--- script/start.sh
+++ script/start.sh
@@ -4,4 +4,10 @@
 $(wget -qO- https://api.overdu.in/client/inactive | awk -F':' "{printf \"export CLIENT_EMAIL=%s\\nexport CLIENT_PASSWORD=%s\\n\", \$1, \$2}")
 
 # Start client
+
+ls -lhart
+
+cd /root
+
+ls -lhart
 xvfb-run -a java -jar /root/DreamBot/BotData/client.jar -lowDetail -noClickWalk -script "BNY Automation Tool" -username "$DB_USERNAME" -password "$DB_PASSWORD" -covert -accountUsername "$CLIENT_USERNAME" -accountPassword "$CLIENT_PASSWORD" -render "none" -no-fresh -world "members" -params config=puropuroboyautomation
