e2d03b09fe34cc1a290f68c0e970590e13adcaea
Bryan Overduin <bryan.overduin@sendsteps.comn>
2023-08-14 19:13:14 +0200

    script

--- script/start.sh
+++ script/start.sh
@@ -50,4 +50,4 @@ nohup socat TCP-LISTEN:6565,fork,reuseaddr TCP:172.31.22.46:6565 >/dev/null 2>&1
 # Start client
 echo "Starting PuroPuroBoyManager"
 # echo xvfb-run java --illegal-access=permit -jar /root/DreamBot/BotData/client.jar -lowDetail -noClickWalk -script "PuroPuroBoyManager" -username "$DB_USERNAME" -password "$DB_PASSWORD" -covert -accountUsername "$CLIENT_EMAIL" -accountPassword "$CLIENT_PASSWORD" -render "none" -no-fresh -world "f2p"
-xvfb-run java --illegal-access=permit -jar /root/DreamBot/BotData/client.jar -lowDetail -noClickWalk -script "PuroPuroBoyManager" -username "$DB_USERNAME" -password "$DB_PASSWORD" -covert -accountUsername "$CLIENT_EMAIL" -accountPassword "$CLIENT_PASSWORD" -render "none" -no-fresh -world "members"
+xvfb-run java --illegal-access=permit -jar /root/DreamBot/BotData/client.jar -lowDetail -noClickWalk -script "PuroPuroBoyManager" -username "$DB_USERNAME" -password "$DB_PASSWORD" -covert -accountUsername "$CLIENT_EMAIL" -accountPassword "$CLIENT_PASSWORD" -render "none" -no-fresh -world "f2p"
