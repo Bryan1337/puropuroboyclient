589c4f3db373524b7b329fefd1aa7f760f8643f5
Bryan Overduin <bryan.overduin@sendsteps.comn>
2023-08-17 22:37:31 +0200

    manager

--- script/manager.sh
+++ script/manager.sh
@@ -2,15 +2,16 @@
 
 LOG_DIR="/root/DreamBot/Logs"
 
+for log_file in "$LOG_DIR"/*; do
 
-tail -n 0 -F "$LOG_DIR"/* | while read -r line; do
+    tail -n 0 -F "$log_file" | while read -r line; do
 
-	echo "GOT LINE"
+        echo "GOT LINE from $log_file"
 
-    if echo "$line" | grep -q "DISABLED"; then
+        if echo "$line" | grep -q "DISABLED"; then
 
-		# curl -X POST -d "name=$CLIENT_EMAIL"
-
-        echo "Account marked as DISABLED";
-    fi
+            echo "Account marked as DISABLED in $log_file"
+            # curl -X POST -d "name=$CLIENT_EMAIL"
+        fi
+    done
 done
