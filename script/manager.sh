5e918cde700bbd243b454402e31c332b1a1df46b
Bryan Overduin <bryan.overduin@sendsteps.comn>
2023-08-17 23:56:10 +0200

    manager

--- script/manager.sh
+++ script/manager.sh
@@ -5,16 +5,11 @@ LOG_DIR="/root/DreamBot/Logs"
 # Start watching for new files and changes in the directory
 inotifywait -m -e create -e modify "$LOG_DIR" | while read -r event; do
 
-	echo "NE $event"
-
     file_path=$(echo "$event" | awk '{print $3}')  # Get the path of the changed/created file
 
-	echo "FP $file_path"
+    if [ -f "$LOG_DIR/$file_path" ]; then  # Check if the path points to a regular file
 
-    if [ -f "$file_path" ]; then  # Check if the path points to a regular file
-
-
-        new_line=$(tail -n 1 "$file_path")  # Read the last line of the file
+        new_line=$(tail -n 1 "$LOG_DIR/$file_path")  # Read the last line of the file
 
 		echo "NL $new_line"
 
